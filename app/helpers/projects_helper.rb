5418d11944c345c85a1f22848c09334d1e4ea15b
Łukasz Ostrowski <kontakt@lostrowski.pl>
2015-02-23 16:52:00 +0100

    project helpers

--- app/helpers/projects_helper.rb
+++ app/helpers/projects_helper.rb
@@ -1,2 +1,13 @@
 module ProjectsHelper
+  def collect_projects
+    Project.all.map{|d| [d.short_name, d.id]}
+  end
+
+  def collect_kinds
+    Project.kinds.map{|k,v| k}
+  end
+
+  def collect_users
+    User.all.map{|u| [u.full_name, u.id]}
+  end
 end
