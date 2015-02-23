89f8a020d09e6e04109d74d744c245e5988f8281
Łukasz Ostrowski <kontakt@lostrowski.pl>
2015-02-23 16:41:53 +0100

    project new & destroy

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -3,4 +3,23 @@ class ProjectsController < ApplicationController
   def index
     @projects = Project.all
   end
+
+  def new
+    @project = Project.new
+  end
+
+  def create
+
+  end
+
+  def destroy
+    @project = Project.find(params[:id])
+    if @project.destroy
+      flash[:success] = 'Projekt został usunięty.'
+      redirect_to project_path
+    else
+      flash[:danger] = 'Podczas usuwania występił błąd'
+      redirect_to project_path
+    end
+  end
 end
