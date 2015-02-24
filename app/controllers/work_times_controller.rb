a0a9955ab8d366bd1f35cbe467b162b0f4bcee58
Łukasz Ostrowski <kontakt@lostrowski.pl>
2015-02-24 23:32:48 +0100

    order work_times

--- app/controllers/work_times_controller.rb
+++ app/controllers/work_times_controller.rb
@@ -1,5 +1,6 @@
 class WorkTimesController < ApplicationController
   def index
-    @work_times = current_user.work_times
+    @work_times = current_user.work_times.order('date DESC', :id)
+    @work_time  = current_user.work_times.new
   end
 end
