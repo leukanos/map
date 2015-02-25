5b671309c0d260fcc5ed530b0611d03c85b68b27
Łukasz Ostrowski <kontakt@lostrowski.pl>
2015-02-25 23:21:49 +0100

    realization bar

--- app/helpers/work_times_helper.rb
+++ app/helpers/work_times_helper.rb
@@ -1,2 +1,15 @@
 module WorkTimesHelper
+  def progress_bar_class
+
+    r = current_user.percentage_realization.to_f
+    if r < 15
+      "progress-bar-danger"
+    elsif r >= 15 && r < 45
+      "progress-bar-warning"
+    elsif r >= 45 && r < 85
+      "progress-bar-info"
+    else
+      "progress-bar-success"
+    end
+  end
 end
