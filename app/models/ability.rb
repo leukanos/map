5ec0176c6b645d8bfa2eb369ff9a915e7a81ed1a
Łukasz Ostrowski <kontakt@lostrowski.pl>
2015-02-25 22:36:57 +0100

    typo

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -9,7 +9,7 @@ class Ability
         can :manage, :all
       else
         can :read, :all
-        cam :manage, WorkTime
+        can :manage, WorkTime
         if user.manager?
           can :manage, Dictionary
           can :manage, Project, assigned_to_id: user.id
