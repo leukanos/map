7d6660d3c31569597e8ef0244d8f979e7ceea5d5
Łukasz Ostrowski <kontakt@lostrowski.pl>
2015-03-02 22:24:57 +0100

    formating

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -2,19 +2,19 @@ class Ability
   include CanCan::Ability
 
   def initialize(user)
-    # Define abilities for the passed in user here. For example:
-    #
-      user ||= User.new # guest user (not logged in)
-      if user.admin?
-        can :manage, :all
-      else
-        can :read, :all
-        can :manage, WorkTime
-        if user.manager?
-          can :manage, Dictionary
-          can :manage, User, id: user.id
-          can :manage, Project, assigned_to_id: user.id
-        end
+    user ||= User.new # guest user (not logged in)
+
+    if user.admin?
+      can :manage, :all
+    else
+      can :read, :all
+      can :manage, WorkTime
+
+      if user.manager?
+        can :manage, Dictionary
+        can :manage, User, id: user.id
+        can :manage, Project, assigned_to_id: user.id
       end
+    end
   end
 end
