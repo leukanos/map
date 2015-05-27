4855298f8747b68b8a0b76ee238214304f98d13f
Łukasz Ostrowski <kontakt@lostrowski.pl>
2015-05-27 23:09:13 +0200

    configure rails_helper & factorygirl

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -3,6 +3,13 @@ ENV['RAILS_ENV'] ||= 'test'
 require 'spec_helper'
 require File.expand_path('../../config/environment', __FILE__)
 require 'rspec/rails'
+require 'simplecov'
+require 'database_cleaner'
+SimpleCov.start
+
+Dir[Rails.root.join("spec/support/**/*.rb")].each { |f| require f }
+
+include ActionDispatch::TestProcess
 # Add additional requires below this line. Rails is not loaded until this point!
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
