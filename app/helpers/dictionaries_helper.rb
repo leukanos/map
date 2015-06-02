0c2f236d79fd3d5b94a8f1c0248e12550e65ecc4
Łukasz Ostrowski <kontakt@lostrowski.pl>
2015-06-03 01:55:49 +0200

    remove dictionaries

--- app/helpers/dictionaries_helper.rb
+++
@@ -1,9 +0,0 @@
-module DictionariesHelper
-  def collect_dictionary_parents(current_id)
-    Dictionary.without_current(current_id).map{|d| [d.symbol, d.id]}
-  end
-
-  def collect_kinds
-    Dictionary.kinds.map{|k,_v| k}
-  end
-end
--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,8 @@ Rails.application.routes.draw do
   devise_for :users, :path_prefix => 'd'
 
   root 'work_times#index'
+  
   resources :users
-  resources :dictionaries
   resources :projects
   resources :work_times
 end
