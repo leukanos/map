133270b36479fac30ede15cac489fa7af748dacf
Łukasz Ostrowski <kontakt@lostrowski.pl>
2015-06-02 23:53:56 +0200

    add description field to projects

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150224151717) do
+ActiveRecord::Schema.define(version: 20150602214519) do
 
   create_table "dictionaries", force: :cascade do |t|
     t.integer  "kind",       limit: 4
@@ -31,8 +31,9 @@ ActiveRecord::Schema.define(version: 20150224151717) do
     t.date     "closing_date"
     t.string   "ancestry",       limit: 255
     t.integer  "assigned_to_id", limit: 4
-    t.datetime "created_at",                 null: false
-    t.datetime "updated_at",                 null: false
+    t.datetime "created_at",                   null: false
+    t.datetime "updated_at",                   null: false
+    t.text     "description",    limit: 65535
   end
 
   create_table "projects_users", force: :cascade do |t|
