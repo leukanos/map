61f999d6f46ccf1123656c96eb5b834447762c7d
Łukasz Ostrowski <kontakt@lostrowski.pl>
2015-06-09 16:12:26 +0200

    some comment

--- app/helpers/work_times_helper.rb
+++ app/helpers/work_times_helper.rb
@@ -1,6 +1,7 @@
 module WorkTimesHelper
-  def progress_bar_class
 
+  #TODO refactor to decorator
+  def progress_bar_class
     r = current_user.percentage_realization.to_f
     if r < 15
       "progress-bar-danger"
